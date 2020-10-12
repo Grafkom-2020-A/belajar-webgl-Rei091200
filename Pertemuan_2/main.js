3b73926733d73c61afd57f9cb2e3a21129bded56
Reihan Nanda Ramadhan <reihannanda2000@gmail.com>
2020-10-12 09:14:23 +0700

    Berhasil buat segitiga dengan gl.TRIANGLES

--- Pertemuan_2/main.js
+++ Pertemuan_2/main.js
@@ -9,7 +9,9 @@ function main() {
     var vertices = [-0.5, 0.5, // Titik A 
         -0.5, -0.5, // Titik B
         0.5, -0.5, // Titik C
-        0.5, 0.5 // Titik D
+        0.5, -0.5, // Titik C
+        0.5, 0.5, // Titik D
+        -0.5, 0.5 // Titik A 
     ];
 
     var positionBuffer = gl.createBuffer();
@@ -42,10 +44,10 @@ function main() {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(100, 0, canvas.height, canvas.height);
 
-
-    var primitive = gl.TRIANGLE_STRIP;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var count = 4; // Jumlah verteks yang akan digambar
+    var count = 6; // Jumlah verteks yang akan digambar
     gl.drawArrays(primitive, offset, count);
 }
